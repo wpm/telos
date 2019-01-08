616e143b45d1ca9c54a1d7aeb094e419d72eafcf
Bill McNeill <wmcneill@sparkcognition.com>
2019-01-08 13:21:41 -0600

    Generate random sequences from Markov model

--- simplelstm.py
+++ simplelstm.py
@@ -1,39 +1,32 @@
-from random import randint
-from typing import List
+from random import choices
+from typing import Iterable, Tuple
+
+from cytoolz.itertoolz import take
+from numpy import array
 
 
-def structured_sequence(n: int = 10, m: int = 9, r: int = 3, l: int = 2) -> List[int]:
-    """
-    Generate a sequence of n digits from 0 to m with sequential structure.
-    """
-
-    def sub_sequence() -> List[int]:
-        t = [i]
-        for _ in range(l):
-            t.append(t[-1] + randint(0, 1))
-        return t
-
-    s = []
-    i = 2 * randint(0, r)
-    for _ in range(n // (l + 1) + 1):
-        s.extend(sub_sequence())
-    return [i % (m + 1) for i in s][:n]
-
-
-def random_sequence(n: int = 10, m: int = 9) -> List[int]:
-    """
-    Generate a random sequence of n digits from 0 to m.
-    """
-    return [randint(0, m) for _ in range(n)]
+def sample_markov_model(transition, emission) -> Iterable[Tuple[int, int]]:
+    s = 0
+    while True:
+        s = choices(range(transition.shape[1]), weights=transition[s].flatten())[0]
+        o = choices(range(1, emission.shape[1] + 1), weights=emission[s].flatten())[0]
+        yield s, o
 
 
 def main():
-    print('Structured')
+    transition = array([
+        [0.0, 0.5, 0.5],
+        [0.0, 0.75, 0.25],
+        [0.0, 0.25, 0.75]
+    ])
+    emission = array([
+        [1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.6, 0.1, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.1, 0.6],
+    ])
+
     for _ in range(10):
-        print(structured_sequence())
-    print('Random')
-    for _ in range(10):
-        print(random_sequence())
+        print([f"{chr(ord('A') + s)}:{o}" for s, o in take(10, sample_markov_model(transition, emission))])
 
 
 if __name__ == '__main__':
