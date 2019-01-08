1ea9277ebe7f5e2a4c4c22a3bfb9f5df27ef7ea4
Bill McNeill <wmcneill@sparkcognition.com>
2019-01-08 16:53:58 -0600

    Categorial sequence to sequence learning

--- simplelstm.py
+++ simplelstm.py
@@ -1,60 +1,79 @@
 from random import shuffle, sample
 from typing import Tuple, Callable
 
-from keras import Sequential, Model
-from keras.layers import LSTM, TimeDistributed, Dense
-from numpy import arange, zeros, array
+from numpy import arange, zeros, array, argmax, newaxis
 
 
-def sequence_to_sequence_model(time_steps: int) -> Model:
+def sequence_to_sequence_model(time_steps: int, labels: int, units: int = 16):
+    from keras import Sequential
+    from keras.layers import LSTM, TimeDistributed, Dense
+
     model = Sequential()
-    model.add(LSTM(units=10, input_shape=(time_steps, 1), return_sequences=True))
-    model.add(TimeDistributed(Dense(1)))
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.add(LSTM(units=units, input_shape=(time_steps, 1), return_sequences=True))
+    model.add(TimeDistributed(Dense(labels, activation='softmax')))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
     return model
 
 
-def long_distance_dependencies():
-    xs = arange(10)
-    ys = zeros(10, int)
-    shuffle(xs)
-    i, j = sample(range(10), 2)
-    xs[j] = xs[i]
-    ys[i] = ys[j] = 1
-    return xs, ys
+def labeled_sequences(n: int, sequence_sampler: Callable[[], Tuple[array, array]]) -> Tuple[array, array]:
+    """
+    Create training data for a sequence-to-sequence labeling model.
 
+    The features are an array of size samples * time steps * 1.
+    The labels are a one-hot encoding of time step labels of size samples * time steps * number of labels.
 
-def aligned_sequences(n: int, sequence_sampler: Callable[[], Tuple[array, array]]) -> Tuple[array, array]:
+    :param n: number of sequence pairs to generate
+    :param sequence_sampler: a function that returns two numeric sequences of equal length
+    :return: feature and label sequences
+    """
+    from keras.utils import to_categorical
+
     xs, ys = sequence_sampler()
-    x = zeros((n, xs.shape[0]), int)
-    y = zeros((n, ys.shape[0]), int)
+    assert len(xs) == len(ys)
+    x = zeros((n, len(xs)), int)
+    y = zeros((n, len(ys)), int)
     for i in range(n):
         xs, ys = sequence_sampler()
         x[i] = xs
         y[i] = ys
-    t = x.shape[1]
-    x = x.reshape(x.shape[0], t, 1)
-    y = y.reshape(y.shape[0], t, 1)
+    x = x[:, :, newaxis]
+    y = to_categorical(y)
     return x, y
 
 
-def main():
-    def reshape(a):
-        return a.reshape(a.shape[0], a.shape[1])
+def digits_with_repetition_labels() -> Tuple[array, array]:
+    """
+    Return a random list of 10 digits from 0 to 9. Two of the digits will be repeated. The rest will be unique.
+    Along with this list, return a list of 10 labels, where the label is 0 if the corresponding digits is unique and 1
+    if it is repeated.
 
+    :return: digits and labels
+    """
+    n = 10
+    xs = arange(n)
+    ys = zeros(n, int)
+    shuffle(xs)
+    i, j = sample(range(n), 2)
+    xs[i] = xs[j]
+    ys[i] = ys[j] = 1
+    return xs, ys
+
+
+def main():
     # Train
-    x, y = aligned_sequences(1000, long_distance_dependencies)
-    model = sequence_to_sequence_model(x.shape[1])
+    x, y = labeled_sequences(10000, digits_with_repetition_labels)
+    model = sequence_to_sequence_model(x.shape[1], y.shape[2])
     model.summary()
-    model.fit(x, y, epochs=20, verbose=2)
+    model.fit(x, y, epochs=500, verbose=2)
     # Test
-    x, y = aligned_sequences(5, long_distance_dependencies)
+    x, y = labeled_sequences(5, digits_with_repetition_labels)
     y_ = model.predict(x, verbose=0)
-    x, y, y_ = reshape(x), reshape(y), reshape(y_)
+    x = x[:, :, 0]
     for i in range(x.shape[0]):
         print(' '.join(str(n) for n in x[i]))
-        print(' '.join([' ', '*'][n] for n in y[i]))
-        print(y_[i])
+        print(' '.join([' ', '*'][int(argmax(n))] for n in y[i]))
+        print(' '.join([' ', '*'][int(argmax(n))] for n in y_[i]))
+        print()
 
 
 if __name__ == '__main__':
